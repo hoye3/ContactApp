088653121fc911ca6268fd3d233cc36a0cd872bb
hoye3 <sgames12@gmail.com>
2017-05-15 09:31:45 -0700

    Stephanie Clifner - more stuff added
    Added adress and number, toast message working, interface mostly
    complete...

--- ContactApp/app/src/main/java/com/example/clifners6171/contactapp/DatabaseHelper.java
+++ ContactApp/app/src/main/java/com/example/clifners6171/contactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.clifners6171.contactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,6 +14,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "NUMBER";
+    public static final String COL_4 = "ADDRESS";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -28,4 +31,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+
+    public boolean insertData(String name, String number, String address) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, number);
+        contentValues.put(COL_4, address);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
+    }
+
 }
--- ContactApp/app/src/main/java/com/example/clifners6171/contactapp/MainActivity.java
+++ ContactApp/app/src/main/java/com/example/clifners6171/contactapp/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.clifners6171.contactapp;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
     DatabaseHelper myDb;
-    EditText editName;
+    EditText editName, editNumber, editAddress;
     Button btnAddData;
 
     @Override
@@ -21,5 +25,35 @@ public class MainActivity extends ActionBarActivity {
 
         myDb = new DatabaseHelper(this);
 
+        editName = (EditText) findViewById(R.id.editText_name);
+        editNumber = (EditText) findViewById(R.id.editText_number);
+        editAddress = (EditText) findViewById (R.id.editText_address);
+
+    }
+
+    public void addData (View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editNumber.getText().toString(), editAddress.getText().toString());
+
+        if(isInserted) {
+            Log.d("MyContact", "Data insertion successful");
+            //Create toast message to user indicating data inserted correctly
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            String message = "Data inserted correctly";
+            //Toast toast = new Toast(context, message, duration);
+
+            Toast.makeText(context, message, duration).show();
+        }
+        else {
+            Log.d("MyContact", "Data insertion unsuccessful");
+            //Create toast message to user indicating data inserted incorrectly
+
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            String message = "Data not inserted correctly";
+            //Toast toast = new Toast(context, message, duration);
+
+            Toast.makeText(context, message, duration).show();
+        }
     }
 }
